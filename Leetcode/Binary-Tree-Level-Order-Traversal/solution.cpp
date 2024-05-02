7287d35c3ff25cab95d46f4e277e0bf59d67cfe8
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2024-05-02 21:24:13 +0800

    new solution of BTLOT

--- Leetcode/Binary-Tree-Level-Order-Traversal/solution.cpp
+++ Leetcode/Binary-Tree-Level-Order-Traversal/solution.cpp
@@ -6,33 +6,42 @@ using namespace std;
 
 class Solution {
 public:
+    void levelOrder(TreeNode* root, vector<vector<int>>& ans, int level) {
+        if (!root) return;
+
+        if (level+1 > ans.size()) {
+            ans.push_back({});
+        }
+        ans[level].push_back(root->val);
+
+        levelOrder(root->left, ans, level+1);
+        levelOrder(root->right, ans, level+1);
+    }
     vector<vector<int>> levelOrder(TreeNode* root) {
-        if (!root) return {};
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
 
-        vector<vector<int>> left = levelOrder(root->left);
-        vector<vector<int>> right = levelOrder(root->right);
+        if (root != nullptr) q.push(root);
 
-        int longerSize = max(left.size(), right.size());
-
-        for (int i=0; i<longerSize; i++) {
-            if (i >= left.size()) {
-                left.push_back(right[i]);
-            } else if (i >= right.size()){
-                break;
-            } else {
-                left[i].insert(left[i].end(), right[i].begin(), right[i].end());
+        while (!q.empty()) {
+            int qSize = q.size();
+            vector<int> subV;
+            for (int i=0; i<qSize; i++) {
+                if (q.front()->left != nullptr) q.push(q.front()->left);
+                if (q.front()->right != nullptr) q.push(q.front()->right);
+                subV.push_back(q.front()->val);
+                q.pop();
             }
+            ans.push_back(subV);
         }
 
-        left.insert(left.begin(), {root->val});
-
-        return left;
+        return ans;
     }
 };
 
 int main() {
-    // TreeNode* root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
-    TreeNode* root = nullptr;
+    TreeNode* root = new TreeNode(3, new TreeNode(9), new TreeNode(20, new TreeNode(15), new TreeNode(7)));
+    // TreeNode* root = nullptr;
     Solution solution;
 
     vector<vector<int>> ans = solution.levelOrder(root);
