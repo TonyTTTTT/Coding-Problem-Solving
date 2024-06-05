0ab06bf7427c36a05a6cc721bf497b37c78492fd
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2024-06-05 21:40:13 +0800

    complete not solved problem, desktop

--- Leetcode/Maximum-Sum-Circular-Subarray/solution.cpp
+++ Leetcode/Maximum-Sum-Circular-Subarray/solution.cpp
@@ -4,28 +4,49 @@ using namespace std;
 
 class Solution {
 public:
-    int maxSubarraySumCircular(vector<int>& nums) {
-        int startIdx=0, curIdx=0;
-        int ans=INT_MIN, curSum=0;
+    int minSubarray(vector<int>& nums) {
+        int minSoFar = nums[0];
+        int minEndHere=0;
 
-        while (startIdx <= curIdx) {
-            startIdx = curIdx;
-            curSum = 0;
-            while (curSum >= 0) {
-                curSum += nums[curIdx];
-                if (curSum > ans) ans = curSum;
-
-                curIdx = (curIdx+1) % nums.size();
-            }
+        for (int i=0; i<nums.size(); i++) {
+            minEndHere += nums[i];
+            minEndHere = min(minEndHere, nums[i]);
+            minSoFar = min(minSoFar, minEndHere);
         }
 
-        return ans;
+        return minSoFar;
+    }
+
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int maxSoFar=nums[0], maxEndHere=0;
+        int allSum = 0;
+
+        for (int i=0; i<nums.size(); i++) {
+            allSum += nums[i];
+            maxEndHere = max(maxEndHere+nums[i], nums[i]);
+            maxSoFar = max(maxSoFar, maxEndHere);
+        }
+        
+        if (maxSoFar > 0) {
+            int minSoFar=nums[0], minEndHere=0;
+
+            for (int i=0; i<nums.size(); i++) {
+                minEndHere = min(minEndHere+nums[i], nums[i]);
+                minSoFar = min(minSoFar, minEndHere);
+            }
+
+            maxSoFar = max(maxSoFar, allSum - minSoFar);
+        }
+
+        return maxSoFar;
     }
 };
 
 int main() {
     Solution solution;
-    vector<int> nums = {5,-3,5};
+    vector<int> nums = {9,7,-6,7,9,-10,5,9,0,-1};
 
     int ans = solution.maxSubarraySumCircular(nums);
+
+    __asm("nop");
 }
