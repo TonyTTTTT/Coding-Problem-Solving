fb8c2a5f03230962f26cfe280199633ad1574f5d
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2024-06-06 17:23:27 +0800

    another way to solved, desktop

--- Leetcode/Binary-Tree-Right-Side-View/solution.cpp
+++ Leetcode/Binary-Tree-Right-Side-View/solution.cpp
@@ -6,25 +6,40 @@ using namespace std;
 
 class Solution {
 public:
+    void preOrder(TreeNode* root, vector<int>& ans, int lvl) {
+        if (!root) return;
+
+        if (ans.size() == lvl) ans.push_back(root->val);
+
+        preOrder(root->right, ans, lvl+1);
+        preOrder(root->left, ans, lvl+1);
+    }
+
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
-        queue<TreeNode*> q;
-        if (root) q.push(root);
-
-        while (!q.empty()) {
-            int curSize = q.size();
-
-            TreeNode* node;
-            for (int i=0; i<curSize; i++) {
-                node = q.front(); q.pop();
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-            ans.push_back(node->val);
-        }
+        preOrder(root, ans, 0);
 
         return ans;
     }
+    // vector<int> rightSideView(TreeNode* root) {
+    //     vector<int> ans;
+    //     queue<TreeNode*> q;
+    //     if (root) q.push(root);
+
+    //     while (!q.empty()) {
+    //         int curSize = q.size();
+
+    //         TreeNode* node;
+    //         for (int i=0; i<curSize; i++) {
+    //             node = q.front(); q.pop();
+    //             if (node->left) q.push(node->left);
+    //             if (node->right) q.push(node->right);
+    //         }
+    //         ans.push_back(node->val);
+    //     }
+
+    //     return ans;
+    // }
 };
 
 
