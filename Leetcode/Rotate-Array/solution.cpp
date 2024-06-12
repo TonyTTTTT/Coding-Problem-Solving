31445d86345bc34edd16b4a9c9ddca9e03a0191c
TsouTony <a546518a@gmail.com>
2024-06-12 19:05:57 +0800

    new way to solve

--- Leetcode/Rotate-Array/solution.cpp
+++ Leetcode/Rotate-Array/solution.cpp
@@ -1,23 +1,17 @@
 #include <vector>
+#include <algorithm>
 using namespace std;
 
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        vector<int> ans;
         int n = nums.size();
         k = k % n;
         if (k==0) return;
-        
-        int start = n - k;
-        int idx = start;
 
-        do {
-            ans.push_back(nums[idx]);
-            idx = (idx+1) % n;
-        } while (idx != start);
-
-        nums = ans;
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
 
         return;
     }
@@ -25,8 +19,8 @@ public:
 
 int main() {
     Solution solution;
-    vector<int> nums = {1};
-    int k = 0;
+    vector<int> nums = {1,2,3,4,5,6,7};
+    int k = 3;
     
     solution.rotate(nums, k);
 }
